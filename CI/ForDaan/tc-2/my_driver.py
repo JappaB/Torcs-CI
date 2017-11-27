9508120faee9e39a40de59cdefddc7c9b5ae6598
Unknown <stickfiguredaan@gmail.com>
2017-11-27 01:14:03 +0100

    Added previous work with some updates and nicer code. Set car to cruise mode.

--- CI/ForDaan/tc-2/my_driver.py
+++ CI/ForDaan/tc-2/my_driver.py
@@ -9,8 +9,32 @@ import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from sensorscreen import SensorScreen
 
 
+def brakeAbs (brakePressure, frontSlip):
+	absTarget = -0.25
+	absControl = 0.25
+	brakePressure += (frontSlip - absTarget) * absControl
+	brakePressure = np.clip(brakePressure, 0.0, 1.0)
+	return brakePressure
+
+def accelTcl (accelPressure, rearSlip):
+	tclTarget = 0.3
+	tclControl = 0.25
+	accelPressure -= (rearSlip - tclTarget) * tclControl
+	accelPressure = np.clip(accelPressure, 0, 1)
+	return accelPressure
+
+def autoTransmission (gear, rpm, rearSlip):
+	upshiftRpm = 9600
+	downshiftRpm = 3500
+	maximumSlip = 0.1
+	if gear < 6 and rpm > upshiftRpm and rearSlip < maximumSlip:
+		return gear + 1
+	elif gear > 1 and rpm < downshiftRpm:
+		return gear - 1
+	return gear
 
 def load_model():
 	path = os.path.join('models','trainedmodel_bbdata_500hidden_3out(tanh)_22in_500it_adam(hopefully_good_results)_standardmodel_nietdict')
@@ -27,50 +51,111 @@ def state_var(carstate):
 	return inp_data
 
 class MyDriver(Driver):
-	# Override the `drive` method to create your own driver
-	...
-	# def drive(self, carstate: State) -> Command:
-	#     # Interesting stuff
-	#     command = Command(...)
-	#     return command
 
 	def __init__(self,logdata = False):
 		super().__init__(logdata)
-
+		# default angles, will be overwritten by range_finder_angles()
+		self.rangeAngles = [-90, -65, -50, -35, -25, -20, -15, -10, -5, 0, 5, 10, 15, 20, 25, 35, 50, 65, 90]
 		# Load model
-		self.neural_net = load_model()		
+		self.neural_net = load_model()
+		# open sensor data viewer
+		self.sensorScreen = SensorScreen()
+		# remembering gas and brake pedals
+		self.accelPressure = 1.0
+		self.brakePressure = 0.2
+	
+	def on_shutdown(self):
+		...
+
+	@property
+	def range_finder_angles(self):
+		self.rangeAngles = []
+		roadWidth = 11
+		sensorFocus = 25
+		sampleSpread = (np.logspace(0, 1.0, base=sensorFocus, num=9) - 1)/(sensorFocus-1)
+		for i in range(19):
+			if i < 9:
+				angle = np.arctan((sampleSpread[i]*200.0) / (roadWidth/2))
+				angle = ((-math.pi/2.0) + angle) * 180/math.pi
+			elif i == 9:
+				angle = 0
+			elif i > 9:
+				angle = np.arctan((sampleSpread[(9-(i-9))]*200.0) / (roadWidth/2))
+				angle = ((math.pi/2.0) - angle) * 180/math.pi
+			self.rangeAngles.append(angle)
+		return self.rangeAngles
+
+
+	def steerToCenter (self, trackPos):
+		steering = trackPos * -0.50
+		steering = np.clip(steering, -1, 1)
+		return steering
+
+
+	def steerToCorner (self, angle):
+		steering = angle*5 / math.pi
+		return steering
+
+
+	def steerToFurthestRange (self, trackRanges):
+		maxIndex = np.argmax(trackRanges)
+		angle = -self.rangeAngles[maxIndex] * math.pi/180.0
+		steering = angle / math.pi
+		return steering
+
+
+	def cruise (self, carstate):
+		command = Command()
+		command.steering = (
+				self.steerToCorner(carstate.angle) + 
+				self.steerToCenter(carstate.distance_from_center) + 
+				self.steerToFurthestRange(carstate.distances_from_edge))
+		command.gear = autoTransmission(carstate.gear, carstate.rpm, 0.0)
+
+		targetSpeed = 100 # kph
+		targetSpeed -= 100 * abs(command.steering)
+		targetSpeed -= (100 - np.max(carstate.distances_from_edge))/2
+		targetSpeed = max(targetSpeed, 30)
+		targetSpeed /= 3.6 # m/s
+		self.accelPressure += (targetSpeed - carstate.speed_x) * 0.01
+		self.accelPressure = np.clip(self.accelPressure, 0, 0.6)
+		command.accelerator = self.accelPressure
+		command.brake = 0.0
+		if carstate.speed_x > 10 and carstate.speed_x*2 > np.max(carstate.distances_from_edge):
+			command.brake = 0.5
+		return command
+
 
 	def drive(self, carstate)-> Command:
 		"""
-        Produces driving command in response to newly received car state.
+		Produces driving command in response to newly received car state.
 
-        This is a dummy driving routine, very dumb and not really considering a
-        lot of inputs. But it will get the car (if not disturbed by other
-        drivers) successfully driven along the race track.
-        """
-
+		This is a dummy driving routine, very dumb and not really considering a
+		lot of inputs. But it will get the car (if not disturbed by other
+		drivers) successfully driven along the race track.
+		"""
 		command = Command()
+		command = self.cruise(carstate)
 
 
 
 
+		# model_outp = self.neural_net(state_var(carstate))
+		# print('output0',float(model_outp.data[0]))
 
-		model_outp = self.neural_net(state_var(carstate))
-		print('output0',float(model_outp.data[0]))
+		# # command.accelerator = model_outp.data[0]
+		# # command.brake =model_outp.data[1]
+		# # command.steering = model_outp.data[2]
+		# self.steer(carstate, 0.0, command)
+		# # v_x = 80
+		# ACC_LATERAL_MAX = 6400 * 5
+		# v_x = min(80, math.sqrt(ACC_LATERAL_MAX / abs(command.steering)))
 
-		# command.accelerator = model_outp.data[0]
-		# command.brake =model_outp.data[1]
-		# command.steering = model_outp.data[2]
-		self.steer(carstate, 0.0, command)
-		# v_x = 80
-		ACC_LATERAL_MAX = 6400 * 5
-		v_x = min(80, math.sqrt(ACC_LATERAL_MAX / abs(command.steering)))
+		# print('steering command',command.steering)
+		# print('accelerator command', model_outp.data[0])
+		# print('brake command', model_outp.data[1])
 
-		print('steering command',command.steering)
-		print('accelerator command', model_outp.data[0])
-		print('brake command', model_outp.data[1])
-
-		self.accelerate(carstate, v_x, command)
-
+		# self.accelerate(carstate, v_x, command)
+		self.sensorScreen.update(carstate, self.rangeAngles)
 		return command
 
---
+++ CI/ForDaan/tc-2/sensorscreen.py
@@ -0,0 +1,184 @@
+from collections import namedtuple
+from tkinter import *
+import math
+import numpy as np
+
+Point = namedtuple('Point', 'x y')
+Box = namedtuple('Box', 'width height')
+
+class SensorScreen:
+	def __init__ (self):
+		self.trackAngleCompensation = 0.0
+		self.trackPosCompensation = 0.0
+		self.screenSize = Box(600, 1000)
+		self.carSize = Box(1.94, 4.52)
+		self.carCenter = Point(
+				0.5*self.screenSize.width, 
+				0.9*self.screenSize.height)
+		self.zoom = 4.0
+		self.screenElements = {}
+		self.window = Tk()
+		self.canvas = Canvas(
+				self.window, 
+				width=self.screenSize.width, 
+				height=self.screenSize.height)
+		self.canvas.pack()
+		self.createScreenElements()
+
+	def createScreenElements(self):
+		# create screen elements in drawing order
+		# green background
+		element = self.canvas.create_rectangle(
+				0, 
+				0, 
+				self.screenSize.width+1, 
+				self.screenSize.height+1, 
+				fill='#018E0E')
+		self.screenElements['background'] = element
+
+		# grey road
+		element = self.canvas.create_polygon(
+				[0,0,0,0], 
+				fill='#777', 
+				smooth='1')
+		self.screenElements['road'] = element
+
+		# 2 white sidelines of track
+		self.screenElements['sidelines'] = []
+		for i in range(2):
+			element = self.canvas.create_line(
+					self.carCenter, 
+					self.carCenter, 
+					fill='#CCC', 
+					width='3', 
+					smooth='1')
+			self.screenElements['sidelines'].append(element)
+
+		# the 19 faint lines showing the range finders
+		self.screenElements['rangeLines'] = []
+		for i in range(19):
+			element = self.canvas.create_line(
+					self.carCenter, 
+					self.carCenter, 
+					fill='#888')
+			self.screenElements['rangeLines'].append(element)
+
+		# 36 red arcs indicating possible enemy position
+		self.screenElements['enemyArcs'] = []
+		for i in range(36):
+			thetaStart = -math.pi/2 - (i+1) * math.pi/18
+			thetaStart *= 180.0/math.pi
+			extent = 10
+			element = self.canvas.create_arc(
+				self.carCenter, 
+				self.carCenter, 
+				start=thetaStart, 
+				extent=extent, 
+				style='arc', 
+				width='2', 
+				outline='red')
+			self.screenElements['enemyArcs'].append(element)
+
+		# the car
+		self.screenElements['car'] = self.canvas.create_rectangle(
+			self.carCenter.x - 0.5 * self.carSize.width * self.zoom, 
+			self.carCenter.y - 0.5 * self.carSize.height * self.zoom, 
+			self.carCenter.x + 0.5 * self.carSize.width * self.zoom, 
+			self.carCenter.y + 0.5 * self.carSize.height * self.zoom, 
+			fill="blue")
+		#self.screenElements['car'] = self.canvas.create_polygon()
+
+
+	def updateTrackDisplay (self, trackdata, rangeAngles):
+		sidelinePoints = []
+		sidelinePoints.append([]) # left
+		sidelinePoints.append([]) # right
+		roadPoints = []
+		maxIndex = np.argmax(trackdata)
+
+		for i in range(19):
+			# update range lines
+			theta = rangeAngles[i] * math.pi/180.0
+			# keep track angle constant angle in screen
+			theta += self.trackAngleCompensation
+			R = [[np.cos(theta), -np.sin(theta)], [np.sin(theta), np.cos(theta)]];
+			distance = trackdata[i] * self.zoom
+			secondPoint = np.array([0,-distance])
+			secondPoint = (R @ secondPoint) + self.carCenter
+			self.canvas.coords(
+					self.screenElements['rangeLines'][i], 
+					*self.carCenter, 
+					*tuple(secondPoint))
+
+			# save for road surface polygon
+			roadPoints.append(secondPoint)
+
+			# assign it to left or right side of track
+			# left/right are split by the furthest scan
+			if i <= maxIndex and trackdata[i] < 200:
+				sidelinePoints[0].append(secondPoint)
+			elif i > maxIndex and trackdata[i] < 200:
+				sidelinePoints[1].append(secondPoint)
+
+			# draw furthest scan in a different color
+			isFurthestScan = i == maxIndex
+			color = '#888'
+			if isFurthestScan:
+				color = 'blue'
+			self.canvas.itemconfig(
+					self.screenElements['rangeLines'][i], 
+					fill=color)
+
+		# update road polygon
+		magic = [e for l in roadPoints for e in l]
+		self.canvas.coords(
+				self.screenElements['road'], 
+				magic)
+
+		# update white side lines
+		for i in range(2):
+			magic = [e for l in sidelinePoints[i] for e in l]
+			if len(magic) < 4:
+				magic = [*self.carCenter, *self.carCenter]
+			self.canvas.coords(
+					self.screenElements['sidelines'][i], 
+					magic)
+
+		# update background to force redraw
+		self.canvas.coords(
+				self.screenElements['background'], 
+				[0, 0, self.screenSize.width+1, self.screenSize.height+1])
+
+		self.canvas.coords(
+				self.screenElements['car'],
+				self.carCenter.x - 0.5 * self.carSize.width * self.zoom, 
+				self.carCenter.y - 0.5 * self.carSize.height * self.zoom, 
+				self.carCenter.x + 0.5 * self.carSize.width * self.zoom, 
+				self.carCenter.y + 0.5 * self.carSize.height * self.zoom)
+
+	def updateEnemyDisplay (self, opponents):
+		for i in range(36):
+			distance = opponents[i]
+			if distance > 199.9: # no enemy in range
+				distance = 0.0
+			distance *= self.zoom
+			self.canvas.coords(
+					self.screenElements['enemyArcs'][i],
+					self.carCenter.x-distance, 
+					self.carCenter.y-distance,
+					self.carCenter.x+distance, 
+					self.carCenter.y+distance)
+
+	def update (self, carState, rangeAngles):
+		self.trackAngleCompensation = carState.angle * math.pi/180.0
+
+		# keep center of track at constant point on screen
+		# pray track is 11m wide
+		trackOffsetPixels = 5.5 * carState.distance_from_center * self.zoom
+		self.carCenter = Point(
+				0.5*self.screenSize.width - trackOffsetPixels, 
+				0.9*self.screenSize.height)
+		self.updateTrackDisplay(carState.distances_from_edge, rangeAngles)
+		self.updateEnemyDisplay(carState.opponents)
+		if self.window.state() == 'normal':
+			self.canvas.update()
